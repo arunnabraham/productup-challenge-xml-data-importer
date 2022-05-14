fa507f79e71efb0b5c134afb216039ebd57cc210
Arun Abraham <arunnabraham@gmail.com>
2022-05-15 02:23:59 +0530

    file memory optimize

--- sampling/test_xml_reader.php
+++ sampling/test_xml_reader.php
@@ -13,17 +13,18 @@ $reader->close();
 function getNodeData($node): string
 {
     $fp = fopen('php://memory', 'rw');
-    putNodeResultToStream($node,$fp);
+    putNodeResultToStream($node, $fp);
     rewind($fp);
     $str = stream_get_contents($fp);
-    var_dump(explode(PHP_EOL.'[###]', $str));
+    var_dump(explode(PHP_EOL, $str));
     fclose($fp);
     return $str;
 }
 
-function putNodeResultToStream(\DomNode $node,$fp): void
+function putNodeResultToStream(\DomNode $node, $fp): void
 {
     $nodePath = pathinfo($node->getNodePath());
+
     if ($node->nodeType === XML_ELEMENT_NODE) {
         if ($node->hasChildNodes()) {
             $children = getNodeChildren($node->childNodes);
@@ -32,19 +33,48 @@ function putNodeResultToStream(\DomNode $node,$fp): void
                 foreach ($children as $childNode) {
                     putNodeResultToStream($childNode, $fp);
                 }
+                /*
+                SEPARATOR NODE START;
+                 */
+                //rewind($fp);
+                //echo stream_get_contents($fp);
+
+                $nodePathBaseName = pathinfo($node->getNodePath(), PATHINFO_BASENAME);
+               if(!empty($node->nextSibling->nodeName))
+               {
+                   if((int)preg_match('/^(\w+)(\[\d+])$/i',$nodePathBaseName)===0) //parent path ignore
+                   {
+                      /* if($init===0){
+                       echo $node->nodeName; //header
+                       } else {
+                           $node->nodeValue; //rows
+                       } */
+                       fputs($fp,$node->nodeValue.'[:-:]');
+                   } else {
+                       fputs($fp,PHP_EOL);
+                   }
+               }
+                //file_put_contents('text.test', PHP_EOL.stream_get_contents($fp), FILE_APPEND);
+                //ftruncate($fp,0);
+                /*
+                SEPARATOR NODE START;
+                 */
             }
         } else {
-           // $result[$nodePath['basename']] = $node->nodeValue;
-            fputs($fp, $nodePath['basename'] . '|~ => ~|' . $node->nodeValue);
+            // $result[$nodePath['basename']] = $node->nodeValue;
+          /*  if (strlen(trim($node->nodeValue)) > 0) {
+                fputs($fp, $nodePath['basename'] . '|~ => ~|');
+            } */
         }
-        fputs($fp,PHP_EOL.'[####]');
     } else {
-        if ($nodePath['basename'] === 'text()') {
-            $nodePathSplit = explode("/", $nodePath['dirname']);
-            fputs($fp, end($nodePathSplit) . '|~ => ~|' . $node->nodeValue);
-           // $result[end($nodePathSplit)] = $node->nodeValue;
-            unset($nodePathSplit);
-        }
+       /* if ($nodePath['basename'] === 'text()') {
+            if (strlen(trim($node->nodeValue)) > 0) {
+                $nodePathSplit = explode("/", $nodePath['dirname']);
+                fputs($fp, end($nodePathSplit) . '|~ => ~|' . $node->nodeValue);
+                // $result[end($nodePathSplit)] = $node->nodeValue;
+                unset($nodePathSplit);
+            }
+        } */
     }
 }
 
